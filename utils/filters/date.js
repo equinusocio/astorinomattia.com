e885a683ee7802ef91c4ffa988fb9950efad9515
Mattia Astorino <astorino.mattia@gmail.com>
2020-02-09 23:05:58 +0100

    init xity + posthtml

---
+++ utils/filters/date.js
@@ -0,0 +1,29 @@
+const appendSuffix = n => {
+  var s = ['th', 'st', 'nd', 'rd'],
+    v = n % 100
+  return n + (s[(v - 20) % 10] || s[v] || s[0])
+}
+
+module.exports = function(value) {
+  const dateObject = new Date(value)
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  const dayWithSuffix = appendSuffix(dateObject.getDate())
+
+  return `${dayWithSuffix} ${
+    months[dateObject.getMonth()]
+  } ${dateObject.getFullYear()}`
+}
---
+++ utils/transforms/contentParser.js
@@ -0,0 +1,154 @@
+const jsdom = require('@tbranyen/jsdom')
+const slugify = require('slugify')
+const eleventyConfig = require('../../src/_data/config.json')
+const { JSDOM } = jsdom
+
+module.exports = function(content, outputPath) {
+  if (outputPath.endsWith('.html')) {
+    /**
+     * create the document model
+     */
+    const DOM = new JSDOM(content)
+    const document = DOM.window.document
+
+    /**
+     * Get all the images from the post
+     */
+    const images = [...document.querySelectorAll('main article img')]
+    if (images.length) {
+      images.forEach(image => {
+        /**
+         * Set the loading attribute to all
+         * the images to be lazy loaded if supported
+         */
+        image.setAttribute('loading', 'lazy')
+        /**
+         * Replace images with title with figure and figcaption
+         */
+        if (image.hasAttribute('title')) {
+          /**
+           * Create figure and figcaption elements
+           */
+          const figure = document.createElement('figure')
+          const figCaption = document.createElement('figcaption')
+          /**
+           * Set figcaption content from image title
+           * then remove the title attribute
+           */
+          figCaption.innerHTML = `<small>${image.getAttribute('title')}</small>`
+          image.removeAttribute('title')
+          /**
+           * Clone image inside figure
+           * and add the figcaption element
+           */
+          figure.appendChild(image.cloneNode(true))
+          figure.appendChild(figCaption)
+          /**
+           * Replace the original image with title
+           * with the generated figure
+           */
+          image.replaceWith(figure)
+        }
+      })
+    }
+    /**
+     * Get all the headings inside the post
+     */
+    const articleHeadings = [
+      ...document.querySelectorAll(
+        'article h2, article h3, article h4, article h5, article h6'
+      ),
+    ]
+    if (articleHeadings.length) {
+      /**
+       * Create an ahcor element inside each post heading
+       * to link to the secion
+       */
+      articleHeadings.forEach(heading => {
+        // Create the anchor element
+        const anchor = document.createElement('a')
+        // Create the anchor slug
+        const headingSlug = slugify(heading.textContent.toLowerCase())
+        // Set the anchor href based on the generated slug
+        anchor.setAttribute('href', `#${headingSlug}`)
+        // Add class and content to the anchor
+        anchor.classList.add(eleventyConfig.permalinkClass)
+        anchor.innerHTML = '#'
+        // Set the ID attribute with the slug
+        heading.setAttribute('id', `${headingSlug}`)
+        // Appen che anchor element to the heading
+        heading.appendChild(anchor)
+      })
+    }
+    /**
+     * Get all the iframes inside the article
+     * and wrap them inside a class
+     */
+    const articleEmbeds = [...document.querySelectorAll('main article iframe')]
+    if (articleEmbeds.length) {
+      articleEmbeds.forEach(embed => {
+        const wrapper = document.createElement('div')
+        embed.setAttribute('loading', 'lazy')
+        wrapper.classList.add(eleventyConfig.iframesClass)
+        wrapper.appendChild(embed.cloneNode(true))
+        embed.replaceWith(wrapper)
+      })
+    }
+    /**
+     * Get all the code snippets and wrap them
+     * inside a div to apply custom style
+     */
+    const codeSnippets = [...document.querySelectorAll('pre[class^="language"')]
+    if (codeSnippets.length) {
+      codeSnippets.forEach(embed => {
+        const wrapper = document.createElement('div')
+        wrapper.classList.add(eleventyConfig.codeClass)
+        wrapper.appendChild(embed.cloneNode(true))
+        embed.replaceWith(wrapper)
+      })
+    }
+
+    /**
+     * Get all links with explicit href
+     * and add noopener rel value
+     */
+    const links = [...document.querySelectorAll('a[href]')]
+    if (links.length) {
+      links.forEach(link => {
+        /**
+         * For each link found get all the original attributes
+         * and apply them to the custom link element
+         */
+        const externalLink = document.createElement('a')
+        if (link.hasAttributes()) {
+          const linkAttributes = link.attributes
+          for (var i = linkAttributes.length - 1; i >= 0; i--) {
+            externalLink.setAttribute(
+              linkAttributes[i].name,
+              linkAttributes[i].value
+            )
+          }
+        }
+        /**
+         * If the link starts with http or https
+         * appen the "noopener" value to the rel attribute
+         */
+        const getHref = link.getAttribute('href')
+        const currentRel = link.getAttribute('rel')
+        const isExternal =
+          getHref.startsWith('http') || getHref.startsWith('https')
+        if (isExternal) {
+          externalLink.setAttribute(
+            'rel',
+            currentRel ? currentRel + ' noopener' : 'noopener'
+          )
+        }
+        externalLink.innerHTML = link.innerHTML
+        link.replaceWith(externalLink.cloneNode(true))
+      })
+    }
+
+    return '<!DOCTYPE html>\r\n' + document.documentElement.outerHTML
+  }
+  return content
+}
