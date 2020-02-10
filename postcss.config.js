8f60eb6fb04b748d8c75bdfd58c2d35731271aca
Mattia Astorino <astorino.mattia@gmail.com>
2020-02-10 21:32:05 +0100

    chore: add postcss-modules

--- postcss.config.js
+++ postcss.config.js
@@ -5,17 +5,10 @@ const modulesPath = '.cssModules';
 module.exports = {
   plugins: [
     require('postcss-modules')({
-      camelCase: true,
       getJSON: function(cssFileName, json, outputFileName) {
         !fs.existsSync(modulesPath) && fs.mkdirSync(modulesPath);
         fs.writeFileSync(`./${modulesPath}/cssClasses.json`, JSON.stringify(json));
       }
-      // getJSON: function(cssFileName, json, outputFileName) {
-      //   !fs.existsSync(modulesPath) && fs.mkdirSync(modulesPath);
-      //   const cssName = path.basename(cssFileName, '.pcss');
-      //   const jsonFileName = path.resolve(`./${modulesPath}/${cssName}.json`);
-      //   fs.writeFileSync(jsonFileName, JSON.stringify(json));
-      // }
     }),
     require('postcss-preset-env')({
       stage: 0,
