390c4f3055cf1e98e892b530a760a1f0304ee80d
Mattia Astorino <astorino.mattia@gmail.com>
2020-02-23 18:06:45 +0100

    chore: improve animations

--- assets/js/controllers/post-list.js
+++ assets/js/controllers/post-list.js
@@ -6,27 +6,10 @@ export default class extends Controller {
   connect() {
     const posts = this.postTargets;
 
-    const fadeInTopAnimation = [
-      { opacity: '0', transform: 'translateY(50%)'},
-      { opacity: '1', transform: 'translateY(0%)'},
-    ];
-
-    const animationTiming = {
-      duration: 800,
-      iterations: 1,
-      easing: 'cubic-bezier(0.42, 0, 0.58, 1)'
-    }
-
     let observer = new IntersectionObserver(entries => {
       entries.forEach((entry, i) => {
         if (entry.intersectionRatio > 0) {
-          entry.target.animate(
-            [
-              { opacity: '0'},
-              { opacity: '1'},
-            ],
-            animationTiming
-          )
+          entry.target.style.animationPlayState = 'running';
           // cardAnimation.play()
           observer.unobserve(entry.target);
         }
