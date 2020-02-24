328276833d146062c83c3740203c29358ef99d74
Mattia Astorino <astorino.mattia@gmail.com>
2020-02-24 14:32:11 +0100

    chore: update layout

--- assets/js/controllers/post-list.js
+++ assets/js/controllers/post-list.js
@@ -10,7 +10,6 @@ export default class extends Controller {
       entries.forEach((entry, i) => {
         if (entry.intersectionRatio > 0) {
           entry.target.style.animationPlayState = 'running';
-          // cardAnimation.play()
           observer.unobserve(entry.target);
         }
       });
