145a448d1e070bc3e573f78d187c09d7e26c74b6
Mattia Astorino <astorino.mattia@gmail.com>
2020-02-24 19:51:33 +0100

    chore: renamve PostList to GridList to make it reusable

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,14 @@ import { Application } from 'stimulus'
 import Turbolinks from 'turbolinks'
 /* Import controllers */
 import ThemeSwitcher from './controllers/theme-switcher'
-import PostList from './controllers/post-list'
+import GridList from './controllers/grid-list'
 
 /* Init Stimulus library */
 const application = Application.start()
 
 /* Register Stimulus controllers */
 application.register('theme-switcher', ThemeSwitcher)
-application.register('post-list', PostList)
+application.register('post-list', GridList)
 
 /**
  * Init Turbolinks within the site
