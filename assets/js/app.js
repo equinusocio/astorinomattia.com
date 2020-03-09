8d697d52ec82ebea2dd846f2230a94bbc7ead33f
Mattia Astorino <astorino.mattia@gmail.com>
2020-03-09 11:37:38 +0100

    feat: add scrolles to blogpost

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ import Turbolinks from 'turbolinks'
 /* Import controllers */
 import ThemeSwitcher from './controllers/theme-switcher'
 import GridList from './controllers/grid-list'
+import { Scrolles } from 'scrolles';
 
 /* Init Stimulus library */
 const application = Application.start()
@@ -10,6 +11,12 @@ const application = Application.start()
 /* Register Stimulus controllers */
 application.register('theme-switcher', ThemeSwitcher)
 application.register('post-list', GridList)
+
+
+Scrolles({
+  selector: '[data-scrolles]',
+  mode: 'continuous',
+});
 
 /**
  * Init Turbolinks within the site
