cd7540b14e905f5f9db788eafdb95958806e48cf
Mattia Astorino <astorino.mattia@gmail.com>
2020-05-05 18:27:56 +0200

    feat: add auto theme

--- assets/js/controllers/theme-switcher.js
+++ assets/js/controllers/theme-switcher.js
@@ -1,25 +1,36 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ["light", "dark"]
+  static targets = ["light", "dark", "auto"]
 
   connect() {
     const html = document.documentElement;
     const lightSelector = this.lightTarget.dataset.themeName;
     const darkSelector = this.darkTarget.dataset.themeName;
+    const autoSelector = this.autoTarget.dataset.themeName;
     const lightButton = this.lightTarget
     const darkButton = this.darkTarget
+    const autoButton = this.autoTarget
 
     lightButton.addEventListener('click', () => {
       html.dataset.theme = lightSelector;
       lightButton.setAttribute('aria-pressed', 'true')
       darkButton.setAttribute('aria-pressed', 'false')
+      autoButton.setAttribute('aria-pressed', 'false')
     }, false);
 
     darkButton.addEventListener('click', () => {
       html.dataset.theme = darkSelector;
-      darkButton.setAttribute('aria-pressed', 'true')
       lightButton.setAttribute('aria-pressed', 'false')
+      darkButton.setAttribute('aria-pressed', 'true')
+      autoButton.setAttribute('aria-pressed', 'false')
+    }, false);
+
+    autoButton.addEventListener('click', () => {
+      html.dataset.theme = autoSelector;
+      lightButton.setAttribute('aria-pressed', 'false')
+      darkButton.setAttribute('aria-pressed', 'false')
+      autoButton.setAttribute('aria-pressed', 'true')
     }, false);
   }
 }
