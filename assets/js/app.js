d2adc318a9541e731841a1435421fe7440002da0
Mattia Astorino <mattia.astorino@ovalmoney.com>
2020-05-07 09:41:15 +0200

    chore: remove scrolles due to scroll issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,6 @@ import Turbolinks from 'turbolinks'
 /* Import controllers */
 import ThemeSwitcher from './controllers/theme-switcher'
 import GridList from './controllers/grid-list'
-import Scrolles from './controllers/scrolles'
 
 /* Init Stimulus library */
 const application = Application.start()
@@ -11,7 +10,6 @@ const application = Application.start()
 /* Register Stimulus controllers */
 application.register('theme-switcher', ThemeSwitcher)
 application.register('post-list', GridList)
-application.register('scrolles', Scrolles)
 
 /**
  * Init Turbolinks within the site
