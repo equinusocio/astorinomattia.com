27f7900a22f181bd359ddd2fd8807c0609b7eca6
Mattia Astorino <astorino.mattia@gmail.com>
2020-07-01 18:41:02 +0200

    chore: remove GA

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,8 +3,6 @@ import Turbolinks from 'turbolinks'
 /* Import controllers */
 import ThemeSwitcher from './controllers/theme-switcher'
 import GridList from './controllers/grid-list'
-import galite from 'ga-lite'
-
 
 /* Init Stimulus library */
 const application = Application.start()
@@ -18,9 +16,3 @@ application.register('post-list', GridList)
  */
 Turbolinks.start()
 
-galite('create', 'UA-134447939-1', 'auto')
-
-document.addEventListener("turbolinks:load", (event) => {
-  galite("set", "location", event.data.url)
-  galite('send', 'pageview')
-})
