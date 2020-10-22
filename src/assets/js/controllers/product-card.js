338c717f23d3a020bfc6a394b3359fe0907e76d8
Mattia Astorino <equinusocio@users.noreply.github.com>
2020-10-22 12:28:45 +0200

    chore: update checkout

--- src/assets/js/controllers/product-card.js
+++ src/assets/js/controllers/product-card.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
 import { Controller } from 'stimulus'
 import mediumZoom from 'medium-zoom'
 
 export default class extends Controller {
   static targets = ["image"]
+  static Paddle = window.Paddle;
+
+  openCheckout(event) {
+    const prodID = event.target.dataset.id
+    window.Paddle.Checkout.open({
+      product: prodID,
+      coupon: process.env.GLOBAL_COUPON
+    });
+  }
 
   connect() {
     const images = this.imageTargets;
@@ -11,6 +21,5 @@ export default class extends Controller {
       background: 'var(--ne-global-background)',
       margin: 40
     })
-
   }
 }
