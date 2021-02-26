aa59dc3f953c66ecf76749510d0fa39281e229bd
Mattia Astorino <equinusocio@users.noreply.github.com>
2021-02-26 16:27:09 +0100

    feat: add portfolio-card, tweak slider and add portfolio.js

---
+++ data/portfolio.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    title: "Brachetto App",
+    label: "macos app",
+    text: "Replacement icons on your hand with live preview, grid perfection, and persistence.",
+    url: "https://brachetto.techr.team/",
+    color1: "#6102D8",
+    color2: "#0094FF"
+  },
+  {
+    title: "Material Theme",
+    label: "vscode extension",
+    text: "Your dev experience will get a boost and your eyes will be always grateful to you.",
+    url: "https://material-theme.site/",
+    color1: "#A673E8",
+    color2: "#00A695"
+  },
+  {
+    title: "Native Elements",
+    label: "Open source project",
+    text: "Customizable native HTML elements with CSS superpowers.",
+    url: "https://native-elements.dev/#/",
+    color1: "#237994",
+    color2: "#D2856D"
+  },
+  {
+    title: "Moxer Theme",
+    label: "vscode extension",
+    text: "Push your developer experience to another level with this curated dark and light theme.",
+    url: "https://marketplace.visualstudio.com/items?itemName=Equinusocio.moxer-theme",
+    color1: "#144B7F",
+    color2: "#1B1C36"
+  },
+]
