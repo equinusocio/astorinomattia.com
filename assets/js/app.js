c2109b3765f6510af61845d27e2fe6f128c0ba70
Mattia <astorino.mattia@gmail.com>
2021-03-24 12:39:19 +0100

    chore: update buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ import Turbolinks from 'turbolinks'
 /* Import controllers */
 import ThemeSwitcher from './controllers/theme-switcher'
 import AudioFx from './controllers/audio-fx'
+import Button from './controllers/button'
 
 /* Init Stimulus library */
 const application = Application.start()
@@ -11,6 +12,7 @@ const application = Application.start()
 /* Register Stimulus controllers */
 application.register('theme-switcher', ThemeSwitcher)
 application.register('audio-fx', AudioFx)
+application.register('button', Button)
 
 /**
  * Init Turbolinks within the site
---
+++ assets/js/controllers/button.js
@@ -0,0 +1,25 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = ["button"]
+
+  /* Init theme switcher actions */
+  connect() {
+    if (this.hasButtonTarget) {
+      let buttons = this.buttonTargets;
+
+      buttons.forEach(btn => {
+        btn.addEventListener('mousemove', e => {
+          const rect = e.target.getBoundingClientRect();
+
+          let x = e.clientX - rect.left;
+          let y = e.clientY - rect.top;
+
+          btn.style.setProperty('--x', x + 'px');
+          btn.style.setProperty('--y', y + 'px');
+        });
+      });
+
+    }
+  }
+}
