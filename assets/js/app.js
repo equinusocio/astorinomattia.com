374cccaf80247249a44069e113cf01f6d77d6e03
Mattia <astorino.mattia@gmail.com>
2021-04-21 16:36:59 +0200

    chore: remove audio fx

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@ import { Application } from 'stimulus'
 
 /* Import controllers */
 import ThemeSwitcher from './controllers/theme-switcher'
-import AudioFx from './controllers/audio-fx'
 import Button from './controllers/button'
 import Mark from './controllers/mark'
 import Cursor from './controllers/cursor'
@@ -19,7 +18,6 @@ const application = Application.start()
 
 /* Register Stimulus controllers */
 application.register('theme-switcher', ThemeSwitcher)
-application.register('audio-fx', AudioFx)
 application.register('button', Button)
 application.register('mark', Mark)
 application.register('cursor', Cursor)
