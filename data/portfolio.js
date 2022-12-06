24fa8b1e1f5d28304a237e632c4d559efd9fc0b9
Mattia Astorino <astorino.mattia@gmail.com>
2022-12-06 16:02:27 +0100

    chore: cose

--- data/portfolio.js
+++ data/portfolio.js
@@ -6,7 +6,7 @@ module.exports = [
     url: "https://design.wonderflow.ai",
     color1: "#e00000",
     color2: "hotpink"
-  },  
+  },
   {
     title: "Oval Partnership",
     label: "UI Development",
@@ -32,14 +32,6 @@ module.exports = [
     color2: "#005B5B"
   },
   {
-    title: "Brachetto App",
-    label: "macos app",
-    text: "Replacement icons on your hand with live preview, grid perfection, and persistence.",
-    url: "https://brachetto.techr.team/",
-    color1: "#6102D8",
-    color2: "#0094FF"
-  },
-  {
     title: "Material Theme",
     label: "vscode extension",
     text: "Your dev experience will get a boost and your eyes will be always grateful to you.",
@@ -48,27 +40,11 @@ module.exports = [
     color2: "#00A695"
   },
   {
-    title: "Native Elements",
-    label: "Open source project",
-    text: "Customizable native HTML elements with CSS superpowers.",
-    url: "https://native-elements.dev/#/",
-    color1: "#237994",
-    color2: "#D2856D"
-  },
-  {
     title: "Moxer Theme",
     label: "vscode extension",
     text: "Push your developer experience to another level with this curated dark and light theme.",
     url: "https://marketplace.visualstudio.com/items?itemName=Equinusocio.moxer-theme",
     color1: "#144B7F",
     color2: "#1B1C36"
-  },
-  {
-    title: "XITY Starter",
-    label: "Blog Starter",
-    text: "Blog-ready starter based Eleventy, the most powerful static site generator",
-    url: "https://github.com/equinusocio/xity-starter",
-    color1: "#FF1A43",
-    color2: "#8EA1E6"
   },
 ]
