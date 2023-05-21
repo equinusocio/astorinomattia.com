db63c7ad94ce9b13c7c8c6b9bf43eee1e5f3cee4
Mattia Astorino <astorino.mattia@gmail.com>
2023-05-21 10:34:20 +0200

    feat: add compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import compressor from "astro-compressor";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://equinusocio.dev',
+  integrations: [
+    // Compressor must be at the end of the array
+    compressor({
+      fileExtensions: [".css", ".js", ".html", ".xml", ".cjs", ".mjs", ".svg", ".txt", '.riv'],
+      gzip: true,
+      brotli: true
+    })
+  ]
 });
